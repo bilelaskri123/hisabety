00f86f3827373154b4d655965fe3863d8230b24b
askri bilel <askribilel09@gmail.com>
2021-02-20 17:29:25 +0100

    add payment page

--- src/app/paiement/paiement.page.ts
+++ src/app/paiement/paiement.page.ts
@@ -10,57 +10,12 @@ import * as $ from "jquery";
 })
 export class PaiementPage implements OnInit {
 
-  public myInput;
-  public shouldShowCancel;
-
-  imgArray: string[];
-  pagename: any;
-  Types: any;
-  myRating: number;
+ public date = new Date().toDateString();
 
   constructor(private route: ActivatedRoute, private router: Router) { 
-    this.imgArray = ['assets/imgs/camion2.jpeg'];
-    this.Types = 1;
   }
 
   ngOnInit() {
-    this.myRating=4
-    this.route.params.subscribe(params => {
-      console.log(params)
-      this.pagename = params.name
-    })
-  }
-
-  rating(data){}
-
-  booking(name){
-    var params = {
-      name : name
-    }
-    this.router.navigate(['book', params]);
-  }
-  
-  myFunction(id) {
-    console.log(id)
-    var x = document.getElementById(id);
-      $(document).ready(function () {
-        $(x).slideToggle('slow');
-      });
-  }
-
-  notification() {
-    this.router.navigateByUrl('/notifications');
-  }
-
-  onInput(evenet) {
-    console.log(evenet.target.value);
-  }
-
-  onCancel(event) {
-    console.log(event.target.value);
-  }
-
-  async openCalModal() {
    
   }
 
