bd62423247a457af9ce4fb1b0b94f2819e783406
askri bilel <askribilel09@gmail.com>
2021-03-18 13:42:15 +0100

    set buttons

--- src/app/trajet/trajet.page.ts
+++ src/app/trajet/trajet.page.ts
@@ -1,34 +1,33 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
 import * as $ from "jquery";
-import { CategoriesService } from '../services/categories.service';
+import { CategoriesService } from "../services/categories.service";
 
 @Component({
-  selector: 'app-trajet',
-  templateUrl: './trajet.page.html',
-  styleUrls: ['./trajet.page.scss'],
+  selector: "app-trajet",
+  templateUrl: "./trajet.page.html",
+  styleUrls: ["./trajet.page.scss"],
 })
 export class TrajetPage implements OnInit {
-
   public categories = [];
-  stockSName = ['المخزن الرئيسي', "مخزن الشرق", "مخزن طرابلس", "مخزن مصراتة"];
+  stockSName = ["المخزن الرئيسي", "مخزن الشرق", "مخزن طرابلس", "مخزن مصراتة"];
   public listFiltred = [];
-  constructor(private router: Router, private categoriesService: CategoriesService) {}
+  constructor(
+    private router: Router,
+    private categoriesService: CategoriesService
+  ) {}
 
   ngOnInit() {
     this.categories = this.categoriesService.getCategories();
     this.listFiltred = this.categories;
   }
 
-  onChangeProduct(value) {
-
-  }
+  onChangeProduct(value) {}
 
   onSelectChange(value: string) {
     this.listFiltred = this.categories;
-    this.listFiltred = this.listFiltred.filter(category => category.stockName === value);
+    this.listFiltred = this.listFiltred.filter(
+      (category) => category.stockName === value
+    );
   }
-
-
-
 }
